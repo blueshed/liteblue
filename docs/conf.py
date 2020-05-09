d7704c4235ab0fbddfea616fb4f4c2c58a69c89d
Peter Bunyan <pete@blueshed.co.uk>
2020-05-09 09:33:01 +0100

    liteblue version: 0.0.8 → 0.0.9

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Peter Bunyan'
 author = 'Peter Bunyan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------
--- liteblue/__init__.py
+++ liteblue/__init__.py
@@ -2,4 +2,4 @@
 
 from .handlers import context
 
-VERSION = "0.0.8"
+VERSION = "0.0.9"
