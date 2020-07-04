f2ce1af7d4bc4ad0ce03f4cf47e9aeccfc90bcb9
Peter Bunyan <pete@blueshed.co.uk>
2020-07-04 16:39:30 +0100

    liteblue version: 0.0.9 → 0.0.10

--- liteblue/__init__.py
+++ liteblue/__init__.py
@@ -2,4 +2,4 @@
 
 from .handlers import context
 
-VERSION = "0.0.9"
+VERSION = "0.0.10"
--- liteblue/apps/__init__.py
+++ liteblue/apps/__init__.py
@@ -11,7 +11,7 @@ from . import cfg
 
 sys.path.insert(0, os.getcwd())
 
-VERSION = "0.0.9"
+VERSION = "0.0.10"
 
 _NAMESPACE_ = Collection()
 
